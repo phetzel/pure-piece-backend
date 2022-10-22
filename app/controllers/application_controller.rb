ac665f9ef48ef3e168fe8f3434e5e37647587b2e
Phillip <phillip@Phillips-MacBook-Air.local>
2022-10-22 13:31:23 -0700

    Added helpers method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     # protect_from_forgery with: :exception
-
+    include ActionController::Helpers
     helper_method :current_user, :logged_in?
   
     private
